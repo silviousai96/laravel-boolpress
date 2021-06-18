586bfb918744e9924fd16888995a3a80051a7712
silvio usai <berzylius@hotmail.it>
2021-06-18 17:07:26 +0200

    controllers

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -14,6 +15,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+
+        return view('guest.home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->middleware('auth')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
+
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->middleware('auth')
+    ->group(function () {
+        Route::get('/', 'HomeController@index')->name('home');
+    });
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 
 
